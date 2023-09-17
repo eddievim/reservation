08a51b59b6877137cb542755549c31ebda7d2845
eddie <eddievim@foxmail.com>
2023-09-17 14:57:09 +0800

    complate grpc code

---
+++ abi/build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    tonic_build::configure()
+    .out_dir("src/pb")
+    .compile(&["protos/reservation.proto"], &["protos"])
+    .unwrap();
+
+
+    // println!("cargo:rerun-if-changed=src/lib.rs");
+    // println!("cargo:rustc-env=ABI={}", std::env::var("CARGO_PKG_VERSION").unwrap());        
+}
+ 
